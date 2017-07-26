021d668fff1e82c025e0b7a2e0cc170e533bcfbf
Lin Fan <flin@thoughtworks.com>
2017-07-26 22:13:32 +0800

    [PET-00] return 400 when request user not exist

--- src/main/scala/com/thoughtworks/petstore/user/controller/UserController.scala
+++ src/main/scala/com/thoughtworks/petstore/user/controller/UserController.scala
@@ -2,13 +2,12 @@ package com.thoughtworks.petstore.user.controller
 
 import com.thoughtworks.petstore.user.dto.assembler.UserAssembler
 import com.thoughtworks.petstore.user.dto.{ExceptionVo, UserVo, UserWithIdVo}
-import com.thoughtworks.petstore.user.exception.{UserExistsException, UserNameTooLongException, UserPasswordIncorrectException}
+import com.thoughtworks.petstore.user.exception._
 import com.thoughtworks.petstore.user.service.UserService
 import io.swagger.annotations.{ApiOperation, ApiParam}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation._
-import springfox.documentation.annotations.ApiIgnore
 
 
 @RestController
@@ -16,7 +15,7 @@ import springfox.documentation.annotations.ApiIgnore
 class UserController {
 
   @Autowired
-  var userService: UserService = _
+  private var userService: UserService = _
 
   @ApiOperation(value = "Check whether user's password match")
   @RequestMapping(value = Array("/authentication"), method = Array(RequestMethod.GET))
@@ -48,19 +47,11 @@ class UserController {
     UserAssembler.userEntity2UserWithIdVo(userService.createUser(user))
   }
 
-  @ApiIgnore
-  @ApiOperation(value = "Remove all users, for test purpose only !!")
-  @RequestMapping(value = Array(""), method = Array(RequestMethod.DELETE))
-  @ResponseBody
-  def cleanUpDatabase(@RequestParam iKnowItsDangerous: Boolean): String = {
-    if (iKnowItsDangerous && userService.removeAllUser) "OK" else "ERROR"
-  }
-
   @ExceptionHandler(Array(classOf[UserExistsException]))
   @ResponseStatus(HttpStatus.CONFLICT)
   def handleUserAlreadyExistException(e: RuntimeException): ExceptionVo = ExceptionVo(409, e.getMessage)
 
-  @ExceptionHandler(Array(classOf[UserNameTooLongException]))
+  @ExceptionHandler(Array(classOf[UserNameTooLongException], classOf[UserNotExistsException]))
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleUserNameTooLongException(e: RuntimeException): ExceptionVo = ExceptionVo(400, e.getMessage)
 
